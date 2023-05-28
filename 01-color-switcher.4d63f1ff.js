a477f8d242dd03cb98fd22981cbdeeca55d5d9c9
fehmituran <86859577+fehmituran@users.noreply.github.com>
2023-05-28 15:26:37 +0000

    Deploying to gh-pages from @ fehmituran/goit-js-hw-09@f4e66015d1d7a1ab0f4d06248cb6147e60812fa5 🚀

---
+++ 01-color-switcher.4d63f1ff.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=null;function d(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=t}e.disabled=!0,t.addEventListener("click",(function(){a=setInterval(d,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.4d63f1ff.js.map
